19c84bffd1baaaebfa87cf4d9d840a0afc5bfee0
Oleg Zubchenko <RedGreenBlueDiamond@gmail.com>
2019-11-07 18:38:01 +0300

    add reporting from background thread

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, escape, request, redirect, url_for
 from flask_socketio import SocketIO, emit
+import datetime
+import threading
+import time
+import eventlet
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
-socketio = SocketIO(app)
+socketio = SocketIO(app, logger=True, engineio_logger=True)
 
 @app.route('/')
 def index():
@@ -19,11 +23,22 @@ def test_message(message):
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    emit('my response', {'data': 'Connected'})
+    socketio.emit('my response', {'data': str(datetime.datetime.now() )},
+            broadcast=True, namespace='/test')
+    emit('my response', {'data': 'Connected'}, broadcast=True)
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected')
+
+def loop_report_time():
+    while True:
+        print('emit')
+        socketio.emit('my response', {'data': str(datetime.datetime.now() )},
+                broadcast=True, namespace='/test')
+        eventlet.sleep(10)
+
+eventlet.spawn(loop_report_time)
 
 if __name__ == '__main__':
     socketio.run(app)
