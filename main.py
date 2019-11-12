e999cb70a76ce02940734c81a168947033e5466c
Oleg Zubchenko <RedGreenBlueDiamond@gmail.com>
2019-11-13 02:46:56 +0300

    cleanup

--- main.py
+++
@@ -1,5 +0,0 @@
-from pgnotify import await_pg_notifications
-import datetime
-
-for notification in await_pg_notifications('postgresql:///oleg', ['channel1']):
-    print("{}: {}".format(str(datetime.datetime.now()), notification.payload))
